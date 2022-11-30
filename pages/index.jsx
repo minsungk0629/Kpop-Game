f5d4e5d95f0a3c4bd9099679a791cbba7b70538a
이도경 <2dokyoung@gmail.com>
2022-11-30 16:33:30 +0900

    kakakaoshare;

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,13 @@
 import StartPage from "../src/startPage";
 import { useEffect } from "react";
 const Home = () => {
+  const kakaoKey = "db38d8749dd506387432d526a93edf4a";
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      console.log("kakaoKey= ", kakaoKey);
+      window.Kakao.init(kakaoKey);
+    }
+  }, []);
   return (
     <>
       <StartPage />
