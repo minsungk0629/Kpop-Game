82007c8797d2852234b1af0ba2e8da95883fa9cd
minsungk0629 <119934595+minsungk0629@users.noreply.github.com>
2023-04-26 23:31:37 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,14 +11,7 @@ class CustomDocument extends Document {
             integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
             crossOrigin="anonymous"
           />
-          <meta
-            name="naver-site-verification"
-            content="ff3d1d8302047a4ebcd535996e11f5344774992b"
-          />
-          <meta
-            name="naver-site-verification"
-            content="1e08c718c78260bbb48647d935c4fc1c72645bd0"
-          />
+
           <script
             src="https://t1.kakaocdn.net/kakao_js_sdk/2.0.1/kakao.min.js"
             integrity="sha384-eKjgHJ9+vwU/FCSUG3nV1RKFolUXLsc6nLQ2R1tD0t4YFPCvRmkcF8saIfOZNWf/"
